7b5e42573d577ccf1331a75b8b37d7c53d71cf79
najihun <51940925+najihun@users.noreply.github.com>
2023-10-16 23:54:14 +0900

    Update email.tf

--- email.tf
+++ email.tf
@@ -1,15 +1,15 @@
 data "tfe_outputs" "result" {
   organization = var.tf_org
-  workspace = var.workspace
+  workspace = var.tf_workspace
 }
 
 resource "email_email" "example" {
   to = var.email_receiver
   from = var.email_sender
-  subject = "Good News from Terraform - ${var.workspace}"
+  subject = "Good News from Terraform - ${var.tf_workspace}"
   body = <<EOT
    This is a test email sent from Terraform using a custom email provider.
-   The outputs from ${data.tfe_outputs.result.workspace} are
+   The outputs from ${var.tf_workspace} are
    ${data.tfe_outputs.result.values}
   EOT
   smtp_server = "smtp.gmail.com"
