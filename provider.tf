3ed1c549188ede883d561f605a7abf89d3d86635
najihun <51940925+najihun@users.noreply.github.com>
2023-10-16 23:40:20 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,9 +4,20 @@ terraform {
       source = "WATonomous/email"
       version = "0.2.3"
     }
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.49.2"
+    }
   }
 }
 
 provider "email" {}
+
+provider "tfe" {
+  hostname = var.hostname
+  token    = var.token
+  version  = "~> 0.49.2"
+}
+  
 
 
