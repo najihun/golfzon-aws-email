c0333f58b541c570b41d4aaf771d43345943ee50
najihun <51940925+najihun@users.noreply.github.com>
2023-10-17 00:03:04 +0900

    Update email.tf

--- email.tf
+++ email.tf
@@ -7,11 +7,7 @@ resource "email_email" "example" {
   to = var.email_receiver
   from = var.email_sender
   subject = "Good News from Terraform - ${var.tf_workspace}"
-  body = <<EOT
-   This is a test email sent from Terraform using a custom email provider.
-   The outputs from ${var.tf_workspace} are
-   ${data.tfe_outputs.result.values}
-  EOT
+  body = "This is a test email sent from Terraform using a custom email provider.\n The outputs from ${var.tf_workspace} are ${data.tfe_outputs.result.values}"
   smtp_server = "smtp.gmail.com"
   smtp_port = "587"
   smtp_username = var.email_sender
