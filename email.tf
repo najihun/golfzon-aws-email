ca795fc59d4ecbc6509e961ad06689d341c68565
najihun <jihun.na@hashicorp.com>
2023-10-17 00:33:01 +0900

    result edit

--- email.tf
+++ email.tf
@@ -7,7 +7,7 @@ resource "email_email" "example" {
   to = var.email_receiver
   from = var.email_sender
   subject = "Good News from Terraform - ${var.tf_workspace}"
-  body = "This is a test email sent from Terraform using a custom email provider.\n The outputs from ${var.tf_workspace} are ${data.tfe_outputs.result.nonsensitive_values.*}"
+  body = "This is a test email sent from Terraform using a custom email provider.\n The outputs from ${var.tf_workspace} are ${data.tfe_outputs.result.nonsensitive_values.[*]}"
   smtp_server = "smtp.gmail.com"
   smtp_port = "587"
   smtp_username = var.email_sender
