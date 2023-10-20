f470bddcdbec5dcda9923ae1a4c913b66c5f5b87
najihun <51940925+najihun@users.noreply.github.com>
2023-10-20 18:46:43 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,3 @@
-variable "hostname" {
-  type = string
-  default = "app.terraform.io"
-}
-variable "token" {
-  type = string
-}
-variable "tf_org" {
-  type = string
-}
-variable "tf_workspace" {
-  type = string
-}
 variable "email_receiver" {
   type = string
 }
@@ -20,4 +7,25 @@ variable "email_sender" {
 variable "password_sender" {
   type = string
   sensitive = true
+}
+
+variable "db_name" {
+  description = "Unique name to assign to RDS instance"
+}
+
+variable "db_username" {
+  description = "RDS root username"
+}
+
+variable "db_password" {
+  description = "RDS root user password"
+  sensitive   = true
+}
+
+variable "multi_az" {
+  description = "Multi AZ Select"
+}
+
+variable "apply_immediately" {
+  description = "Specifies whether any database modifications are applied immediately"
 }
